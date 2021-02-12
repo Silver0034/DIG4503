873bdd20108a648d9e85ee57b311a0dc32c2578b
Jacob <airraid.mail@gmail.com>
2021-02-12 15:12:06 -0500

    Created Lab4

---
+++ lab4/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Market from './components/market'
+
+function App() {
+	return <Market />
+}
+
+export default App
---
+++ lab4/src/components/market.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import MarketItem from './marketItem'
+
+class Market extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			count: 0
+		}
+	}
+	render() {
+		return (
+			<div>
+				<MarketItem count={this.state.count} />
+				<button
+					onClick={() =>
+						this.setState({
+							count: this.state.count + 1
+						})
+					}
+				>
+					Add Item
+				</button>
+			</div>
+		)
+	}
+}
+
+export default Market
